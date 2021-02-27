72ff68fce2af37122b03218fa6ce85cb4fcb44f6
JesusMaba95 <jesus.martin@iteso.mx>
2021-02-27 16:58:48 -0600

    Creating repo

---
+++ RISCV.v
@@ -0,0 +1,21 @@
+/******************************************************************
+* Description
+*	RISCV TOP MODULE
+* Author:
+*	Jesus Martin Barroso
+* email:
+*	jesus.martin@iteso.mx
+* Date:
+*	20/02/2021
+******************************************************************/
+
+module RISCV
+
+(
+	input clk,
+	input reset,
+	output [7:0]gpio_port_in,
+	output [7:0]gpio_port_out
+);
+
+endmodule
