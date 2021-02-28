9d4da4138120aee48715f51b98bb61a75a128eaf
JesusMaba95 <jesus.martin@iteso.mx>
2021-02-27 19:33:19 -0600

    Adding fsm

---
+++ FSM_Control.v
@@ -0,0 +1,295 @@
+/******************************************************************
+* Description
+*	This is a code for FSM _Control of RISCV_MULTICYCLE
+*	procesor.
+* Version:
+*	1.0
+* Author:
+*	Dr. José Luis Pizano Escalante
+* email:
+*	luispizano@iteso.mx
+* Modified by : 
+	* 	Jesus Martin Barroso  ---> RISCV ARCHITECTURE FSM
+* Date:
+*	20/02/2021
+******************************************************************/
+module FSM_Control(
+input clk,
+input rst,
+input zero,
+input [6:0] opcode,
+input [2:0] Funct3,
+input [6:0] Funct7,
+
+output reg PCWrite,
+output reg AdrSrc,
+output reg MemWrite,
+output reg IRWrite,
+output reg RegWrite,
+output reg [2:0] ImmSrc,
+output reg [1:0] ALUsrcA,
+output reg [1:0] ALUsrcB,
+output reg [2:0] ALUCtrl,
+output reg [1:0] ResultSrc
+
+);
+
+localparam R_type_ARITH = 7'h33;//ADD,SUB,AND,OR,SLL,SLT,SLTU,XOR,SRL,SRA
+localparam I_type_ARITH = 7'h13;//ADDI,SLLI,SLTI,SLTIU,XORI,SRLI,SRALI,ORI,ANDI
+localparam I_type_LW    = 7'h03;//LW
+localparam I_type_JALR  = 7'h67;//JALR
+localparam S_type_SW    = 7'h23;//SW
+localparam J_type       = 7'h6F;//JAL
+localparam B_type       = 7'h63;//BEQ,BNE
+localparam U_type       = 7'h17;//AUIPC
+
+localparam S0_FETCH      = 4'b0000;
+localparam S1_DECODE     = 4'b0001;
+localparam S2_MEM_ADDR   = 4'b0010;
+localparam S3_MEM_READ   = 4'b0011;
+localparam S4_MEM_WBA    = 4'b0100;
+localparam S5_MEM_WR     = 4'b0101;
+localparam S6_EXECUTE_R  = 4'b0110;
+localparam S7_ALU_WB     = 4'b0111;
+localparam S8_EXECUTE_I  = 4'b1000;
+localparam S9_JAL        = 4'b1001;
+localparam S10_BEQ       = 4'b1010;
+
+
+reg [3:0] state;
+
+
+
+/********************************************************************************************************************/
+
+always@(posedge clk, negedge rst)
+begin
+       if(!rst)
+		   begin
+			     state <= S0_FETCH;
+			end
+			else
+			   begin
+				    case(state)
+					   S0_FETCH:
+						       state <= S1_DECODE;
+						S1_DECODE:
+						       if(opcode == I_type_LW)
+							    state <= S2_MEM_ADDR;
+								 else if(opcode == S_type_SW)
+							    state <= S2_MEM_ADDR;
+					          else if(opcode == R_type_ARITH)
+							    state <= S6_EXECUTE_R;
+					          else if(opcode == I_type_ARITH)
+							    state <= S8_EXECUTE_I;
+					          else if(opcode == S9_JAL)
+						   	 state <= S7_ALU_WB;
+					          else if(opcode == B_type)
+						   	 state <= S10_BEQ;
+					          else 
+					          state <= S1_DECODE;
+						S2_MEM_ADDR:
+						       if(opcode == I_type_LW)
+							    state <= S3_MEM_READ;
+								 else if(opcode == S_type_SW)
+							    state <= S5_MEM_WR;
+								 else 
+					          state <= S2_MEM_ADDR;
+						S3_MEM_READ:
+								 state <= S4_MEM_WBA;
+						S4_MEM_WBA:
+						       state <= S0_FETCH;
+						S5_MEM_WR:
+						       state <= S0_FETCH;
+						S6_EXECUTE_R:
+			                state <= S7_ALU_WB;
+						S7_ALU_WB:	
+					          state <= S0_FETCH;	
+						S8_EXECUTE_I:
+						       state <= S7_ALU_WB;
+						S9_JAL:
+						       state <= S7_ALU_WB;	
+						S10_BEQ:
+						       state <= S0_FETCH;
+						default: state <= S0_FETCH;
+				  endcase
+				end
+end
+
+always@(*)
+begin
+      case(state)
+		     S0_FETCH:
+			  begin
+					PCWrite   = 1'b1;
+					AdrSrc    = 1'b0;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b1;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'b00;
+					ALUsrcB   = 2'b10;
+					ResultSrc = 2'b00;
+					ALUCtrl   = 3'b010; //ADD
+			  end
+			  S1_DECODE:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'bX;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					if (opcode == I_type_ARITH || opcode == I_type_LW || opcode == I_type_JALR)
+						ImmSrc    = 3'b000;
+					else if (opcode == S_type_SW )
+						ImmSrc    = 3'b001;
+					else if (opcode == B_type )
+						ImmSrc    = 3'b010;
+					else if (opcode == J_type )
+						ImmSrc    = 3'b011;
+					else if (opcode == U_type)
+						ImmSrc    = 3'b100;
+					ALUsrcA   = 2'b01;
+					ALUsrcB   = 2'b01;
+					ResultSrc = 2'bXX;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S2_MEM_ADDR:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'bX;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					if (opcode == I_type_LW)
+						ImmSrc    = 3'b000;
+					else if (opcode == S_type_SW )
+						ImmSrc    = 3'b001;
+					ALUsrcA   = 2'b01;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'b10;
+					ALUsrcB   = 2'b01;
+					ResultSrc = 2'bXX;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S3_MEM_READ:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'b1;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'bXX;
+					ALUsrcB   = 2'bXX;
+					ResultSrc = 2'b10;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S4_MEM_WBA:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'bX;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b1;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'bXX;
+					ALUsrcB   = 2'bXX;
+					ResultSrc = 2'b01;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S5_MEM_WR:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'b1;
+					MemWrite  = 1'b1;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'bXX;
+					ALUsrcB   = 2'bXX;
+					ResultSrc = 2'b10;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+		     S6_EXECUTE_R:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'bX;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'b10;
+					ALUsrcB   = 2'b00;
+					ResultSrc = 2'bXX;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S7_ALU_WB:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'bX;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b1;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'bXX;
+					ALUsrcB   = 2'bXX;
+					ResultSrc = 2'b10;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S8_EXECUTE_I:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'bX;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'b000;;
+					ALUsrcA   = 2'b10;
+					ALUsrcB   = 2'b01;
+					ResultSrc = 2'bXX;
+					ALUCtrl   = 3'b010;
+			  end
+			  S9_JAL:
+			  begin
+					PCWrite   = 1'b1;
+					AdrSrc    = 1'b0;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'b011;;
+					ALUsrcA   = 2'b01;
+					ALUsrcB   = 2'b10;
+					ResultSrc = 2'b10;
+					ALUCtrl   = 3'b010;//ADD
+			  end
+			  S10_BEQ:
+			  begin
+					PCWrite   = 1'b0;
+					AdrSrc    = 1'b0;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b0;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'b10;
+					ALUsrcB   = 2'b00;
+					ResultSrc = 2'b10;
+					ALUCtrl   = 3'b011;//sub
+			  end
+			  default:
+			  begin
+					PCWrite   = 1'b1;
+					AdrSrc    = 1'b0;
+					MemWrite  = 1'b0;
+					IRWrite   = 1'b1;
+					RegWrite  = 1'b0;
+					ImmSrc    = 3'bXXX;
+					ALUsrcA   = 2'b00;
+					ALUsrcB   = 2'b10;
+					ResultSrc = 2'b00;
+					ALUCtrl   = 3'b010; //ADD
+			  end
+		endcase
+end
+
+endmodule
--- RISCV.v
+++ RISCV.v
@@ -38,25 +38,35 @@ wire [31:0]ALUResult_w;
 wire [31:0]ALUResult_ff_w;
 wire [31:0]MuxResult_w;
 wire [2:0] ImmSel_w;
+wire [2:0] ALUCtrl_w;
+wire [1:0] ALUsrcA_w;
+wire [1:0] ALUsrcB_w;
+wire [1:0]ResultSrc_w;
 wire PC_WR_w;
 wire AdrSrc_w;
 wire MemWr_w;
 wire IRWr_w;
 wire RegW_w;
+wire zero_w;
+wire PCWR_w;
+wire AdrSrc_sel_w;
+wire MemWrite_w;
+wire IRWrite_w;
+wire RegWrite_w;
 
 PC_Register
 ProgramCounter
 (
-	//.clk(clk),
-	//.reset(reset),
-	//.enable(PC_WR_w),
+	.clk(clk),
+	.reset(reset),
+	.enable(PCWR_w),
 	.NewPC(MuxResult_w),
 	.PCValue(PC_w)
 );
 
 Mux_2_1 Mux_AdrSrc
 (
-	//.sel(AdrSrc_w),
+	.sel(AdrSrc_sel_w),
 	.In0(PC_w),
 	.In1(MuxResult_w),
 	.Output(Addr_w)
@@ -73,24 +83,24 @@ IDMem
 (
 	.Address(Addr_w),
 	.WriteData(ReadReg2_Data_ff_w),
-	//.MemWrite(MemWr_w),
-	//.clk(clk),
+	.MemWrite(MemWrite_w),
+	.clk(clk),
 	.ReadData(ReadData_w)
 );
 Register OldPc
 (
-  //.clk(clk),
-  //.reset(reset),
-  //.enable(IRWr_w),
+  .clk(clk),
+  .reset(reset),
+  .enable(IRWrite_w),
   .DataInput(PC_w),
   .DataOutput(OldPC_w)
   
 );
 Register Instruction
 (
-  //.clk(clk),
-  //.reset(reset),
-  //.enable(IRWr_w),
+  .clk(clk),
+  .reset(reset),
+  .enable(IRWrite_w),
   .DataInput(ReadData_w),
   .DataOutput(Instruction_w)
   
@@ -99,8 +109,8 @@ Register Instruction
 Register Data
 (
   .clk(clk),
-  //.reset(reset),
-  //.enable(1'b1),
+  .reset(reset),
+  .enable(1'b1),
   .DataInput(ReadData_w),
   .DataOutput(ReadData_w2)
   
@@ -110,9 +120,9 @@ RegisterFile
    .N(DATA_WIDTH)
 ) FILE
 (
-	//.clk(clk),
-	//.reset(rst),
-	//.RegWrite(RegW_w),
+	.clk(clk),
+	.reset(reset),
+	.RegWrite(RegWrite_w),
 	.WriteRegister(Instruction_w[11:7]),
 	.ReadRegister1(Instruction_w[19:15]),
 	.ReadRegister2(Instruction_w[24:20]),
@@ -123,25 +133,25 @@ RegisterFile
 );
 Register Rd_Reg_Data1
 (
-  //.clk(clk),
-  //.reset(reset),
-  //.enable(1'b1),
+  .clk(clk),
+  .reset(reset),
+  .enable(1'b1),
   .DataInput(ReadReg1_Data_w),
   .DataOutput(ReadReg1_Data_ff_w)
   
 );
 Register Rd_Reg_Data2
 (
-  //.clk(clk),
-  //.reset(reset),
-  //.enable(1'b1),
+  .clk(clk),
+  .reset(reset),
+  .enable(1'b1),
   .DataInput(ReadReg2_Data_w),
   .DataOutput(ReadReg2_Data_ff_w)
   
 );
 Mux_3_1 Mux_SrcA
 (
-	//.sel(),
+	.sel(ALUsrcA_w),
 	.In0(PC_w),
 	.In1(OldPC_w),
 	.In2(ReadReg1_Data_ff_w),
@@ -149,7 +159,7 @@ Mux_3_1 Mux_SrcA
 );
 Mux_3_1 Mux_SrcB
 (
-	//.sel(),
+	.sel(ALUsrcB_w),
 	.In0(ReadReg2_Data_ff_w),
 	.In1(imm_w),
 	.In2(32'h4),
@@ -158,33 +168,53 @@ Mux_3_1 Mux_SrcB
 ImmGen ImmGen_i 
 (   
 	.in(Instruction_w[31:7]),
-	//.ImmSel(ImmSel_w),
+	.ImmSel(ImmSel_w),
    .imm(imm_w)
 );
 ALU
 ArithmeticLogicUnit 
 (
-	//.ALUOperation(AluOp_w),
+	.ALUOperation(ALUCtrl_w),
 	.A(SrcA_w),
 	.B(SrcB_w),
-	//.Zero(zero_w2),
+	.Zero(zero_w),
 	.ALUResult(ALUResult_w)
 );
 Register ALu_ff
 (
-  //.clk(clk),
-  //.reset(reset),
-  //.enable(1'b1),
+  .clk(clk),
+  .reset(reset),
+  .enable(1'b1),
   .DataInput(ALUResult_w),
   .DataOutput(ALUResult_ff_w)
   
 );
 Mux_3_1 ResultSrc_Mux
 (
-	//.sel(),
+	.sel(ResultSrc_w),
 	.In0(ALUResult_ff_w),
 	.In1(ReadData_w2),
 	.In2(ALUResult_w),
 	.Output(MuxResult_w)
+);
+FSM_Control	Control
+(
+  .clk(clk),
+  .rst(rst),
+  .zero(zero_w),
+  .opcode(Instruction_w[6:0]),
+  .Funct3(Instruction_w[14:12]),
+  .Funct7(Instruction_w[31:25]),
+  .PCWrite(PCWR_w),
+  .AdrSrc(AdrSrc_sel_w),
+  .MemWrite(MemWrite_w),
+  .IRWrite(IRWrite_w),
+  .RegWrite(RegWrite_w),
+  .ImmSrc(ImmSel_w),
+  .ALUsrcA(ALUsrcA_w),
+  .ALUsrcB(ALUsrcB_w),
+  .ALUCtrl(ALUCtrl_w),
+  .ResultSrc(ResultSrc_w)
+
 );
 endmodule
